ad49dc0f7a8b219dd2074c4e0b3fe13875ff0e2d
joesiewert <joe.siewert@gmail.com>
2015-02-14 20:41:44 -0700

    Updated content security policy

--- config/environment.js
+++ config/environment.js
@@ -17,7 +17,7 @@ module.exports = function(environment) {
       'default-src': "'none'",
       'script-src': "'self'",
       'font-src': "'self'",
-      'connect-src': "'self' http://www.reddit.com",
+      'connect-src': "'self' www.reddit.com",
       'img-src': "'self'",
       'style-src': "'self'",
       'media-src': "'self'"
